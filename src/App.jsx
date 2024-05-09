1a73cfe0efa58194a762ec5d26e7e16aa2521fb5
djmhim <djmhim@gmail.com>
2024-05-09 06:01:43 +0400

    001 MQ

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-800 text-white text-2xl">Hello World</h1>
+      <div className='bg-cyan-800 text-white'>Welcome to my code</div>
+      <input type="text" placeholder='I am input' className='border-2 px-5 py-2 rounded-full mt-5 ml-3' />
+    </>
+  )
+}
+
+export default App
